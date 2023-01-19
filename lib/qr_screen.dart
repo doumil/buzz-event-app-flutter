d3df37ed6687f4e044f4e35435f033fc4eefb910
doumil <93338229+doumil@users.noreply.github.com>
2023-01-19 14:39:06 +0100

    design qr codde scanner 2

--- lib/qr_screen.dart
+++ lib/qr_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:assessment_task/profils_enregistr%C3%A9s.dart';
 import 'package:assessment_task/utils/database_helper.dart';
 import 'package:flutter/foundation.dart';
@@ -30,12 +32,38 @@ class QrcodeScreen extends StatefulWidget {
 }
 
 class _QrcodeScreenState extends State<QrcodeScreen> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(Duration(milliseconds:20), (Timer t) => moved());
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+  moved(){
+    setState(() {
+      if (bottom == 1 || top == 0) {
+        bottom = 0;
+        top = 1;
+      } else if (top == 1 || bottom == 0) {
+        top = 0;
+        bottom = 1;
+      }
+    });
+  }
+  var bottom = 1;
+  var top = 0;
   var response;
-  var updated="";
+  var updated = "";
   var db = new DatabaseHelper();
   String _data = "";
   List<String> litems = [];
-  Userscan user1 = Userscan('','','','','','','','','','','');
+  Userscan user1 = Userscan('', '', '', '', '', '', '', '', '', '', '');
   late SharedPreferences prefs;
   MobileScannerController cameraController = MobileScannerController();
   bool _screenOpened = false;
@@ -55,148 +83,169 @@ class _QrcodeScreenState extends State<QrcodeScreen> {
               color: Colors.black.withOpacity(0.3),
               width: MediaQuery.of(context).size.width,
               height: 100,
+              //MediaQuery.of(context).size.height,
               //margin: EdgeInsets.only(top:60),
               child: Center(
                 child: Text('Scanner',
-                style: TextStyle(fontSize: 18,fontWeight:FontWeight.bold,color: Colors.white)),
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
               ),
             ),
           ),
           Align(
-            alignment: Alignment.center,
-            child: Stack(
-              children: [
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(color: Color(0xff682062), width: 8),
-                      left: BorderSide(color: Colors.white, width: 8),
-
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
+              alignment: Alignment.center,
+              child: Stack(
+                children: [
+                  Container(
                     width: 40,
                     height: 40,
                     decoration: BoxDecoration(
                       border: Border(
-                        bottom: BorderSide(color: Color(0xff682062), width: 8),
-                        right: BorderSide(color: Colors.white, width: 8),
+                        top: BorderSide(color: Color(0xff682062), width: 8),
+                        left: BorderSide(color: Colors.white, width: 8),
                       ),
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: Container(
-                    width: 40,
-                    height: 40,
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom:
+                              BorderSide(color: Color(0xff682062), width: 8),
+                          right: BorderSide(color: Colors.white, width: 8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Colors.white, width: 8),
+                          right: BorderSide(color: Color(0xff682062), width: 8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.white, width: 8),
+                          left: BorderSide(color: Color(0xff682062), width: 8),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    height: 200,
+                    width: 200,
+                    margin: EdgeInsets.symmetric(
+                      vertical: 16,
+                      horizontal: 16,
+                    ),
                     decoration: BoxDecoration(
-                      border: Border(
-                        top: BorderSide(color: Colors.white, width: 8),
-                        right: BorderSide(color: Color(0xff682062), width: 8),
-                      ),
+                        // color: Colors.red.withOpacity(1.0),
+                        ),
+                    child: Stack(
+                      children: [
+                        AnimatedPositioned(
+                            duration: const Duration(seconds: 1),
+                            curve: Curves.fastOutSlowIn,
+                            bottom: bottom == 0 ? 0 : null,
+                            top: top == 0 ? 0 : null,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 1.32,
+                                color: Colors.black,
+                                child: Text("test"),
+                              ),
+                            )),
+                      ],
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  child: Container(
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(color: Colors.white, width: 8),
-                        left: BorderSide(color: Color(0xff682062), width: 8),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(15),
-                  height: 200,
-                  width: 200,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 16,
-                    horizontal: 16,
-                  ),
-                  decoration: BoxDecoration(
-                   // color: Colors.red.withOpacity(1.0),
-                  ),
-                  child: Container(),
-                ),
-              ],
-            )
-            ),
+                ],
+              )),
           Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              color: Colors.black.withOpacity(0.5),
-              child: ButtonBar(
-                alignment:MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    IconButton(
-                      color: Colors.white,
-                      icon: ValueListenableBuilder(
-                        valueListenable: cameraController.torchState,
-                        builder: (context, state, child) {
-                          switch (state as TorchState) {
-                            case TorchState.off:
-                              return const Icon(Icons.flash_off, color: Colors.grey);
-                            case TorchState.on:
-                              return const Icon(Icons.flash_on, color: Colors.white);
-                          }
-                        },
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
+                child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      IconButton(
+                        color: Colors.white,
+                        icon: ValueListenableBuilder(
+                          valueListenable: cameraController.torchState,
+                          builder: (context, state, child) {
+                            switch (state as TorchState) {
+                              case TorchState.off:
+                                return const Icon(Icons.flash_off,
+                                    color: Colors.grey);
+                              case TorchState.on:
+                                return const Icon(Icons.flash_on,
+                                    color: Colors.white);
+                            }
+                          },
+                        ),
+                        iconSize: 32.0,
+                        onPressed: () => cameraController.toggleTorch(),
                       ),
-                      iconSize: 32.0,
-                      onPressed: () => cameraController.toggleTorch(),
-                    ),
-                    IconButton(
-                      color: Colors.white,
-                      icon: ValueListenableBuilder(
-                        valueListenable: cameraController.cameraFacingState,
-                        builder: (context, state, child) {
-                          switch (state as CameraFacing) {
-                            case CameraFacing.front:
-                              return const Icon(Icons.camera_front);
-                            case CameraFacing.back:
-                              return const Icon(Icons.camera_rear);
-                          }
-                        },
+                      IconButton(
+                        color: Colors.white,
+                        icon: ValueListenableBuilder(
+                          valueListenable: cameraController.cameraFacingState,
+                          builder: (context, state, child) {
+                            switch (state as CameraFacing) {
+                              case CameraFacing.front:
+                                return const Icon(Icons.camera_front);
+                              case CameraFacing.back:
+                                return const Icon(Icons.camera_rear);
+                            }
+                          },
+                        ),
+                        iconSize: 32.0,
+                        onPressed: () => cameraController.switchCamera(),
                       ),
-                      iconSize: 32.0,
-                      onPressed: () => cameraController.switchCamera(),
-                    ),
-                    IconButton(
-                        color:Colors.white,
-                        icon:Icon(Icons.close),
-                        onPressed:() {
+                      IconButton(
+                        color: Colors.white,
+                        icon: Icon(Icons.close),
+                        onPressed: () {
                           Navigator.pop(context);
                         },
-                    ),
-                  ]
-              ),
-            )
-          ),
+                      ),
+                    ]),
+              )),
         ],
       ),
     );
   }
-  void _foundBarcode(Barcode barcode, MobileScannerArguments? args) async{
+
+  void _foundBarcode(Barcode barcode, MobileScannerArguments? args) async {
     /// open screen
     if (!_screenOpened) {
       final String code = barcode.rawValue ?? "-1";
       print(code);
       debugPrint('Barcode found! $code');
       _screenOpened = true;
-      int _count=0;
+      int _count = 0;
       prefs = await SharedPreferences.getInstance();
       prefs.setString("Data", code.toString());
       if (code != '-1') {
@@ -228,49 +277,38 @@ class _QrcodeScreenState extends State<QrcodeScreen> {
           print(response);
           print("------------------------------");
           if (response.toString() == "[]") {
-            Navigator.push(
-                context,
+            Navigator.push(context,
                 MaterialPageRoute(builder: (context) => DetailsScreen()));
-          }
-          else if (response.toString() != "[]") {
+          } else if (response.toString() != "[]") {
             user1.evolution = response[0]["evolution"];
             user1.action = response[0]["action"];
             user1.notes = response[0]["notes"];
             user1.created = response[0]["created"];
-            user1.updated = "${DateTime
-                .now()
-                .day}/${DateTime
-                .now()
-                .month}/${DateTime
-                .now()
-                .year} ${DateTime
-                .now()
-                .hour}:${DateTime
-                .now()
-                .minute}";
+            user1.updated =
+                "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year} ${DateTime.now().hour}:${DateTime.now().minute}";
             db.updateUser(user1, user1.email);
-            Navigator.push(context,
+            Navigator.push(
+                context,
                 MaterialPageRoute(
                     builder: (context) => profilsEnregistresScreen()));
           }
-        }
-        else{
+        } else {
           showDialog<String>(
             context: context,
             builder: (BuildContext context) => AlertDialog(
               title: const Text('erreur'),
-              content:  Text('QR code format invalid\n\n'),
+              content: Text('QR code format invalid\n\n'),
               actions: <Widget>[
                 TextButton(
                   onPressed: () => Navigator.pop(context, 'OK'),
-                  child:
-                  const Text('OK', style: TextStyle(color: Color(0xff803b7a))),
+                  child: const Text('OK',
+                      style: TextStyle(color: Color(0xff803b7a))),
                 ),
               ],
             ),
           );
         }
-      }else {
+      } else {
         showDialog<String>(
           context: context,
           builder: (BuildContext context) => AlertDialog(
@@ -280,21 +318,17 @@ class _QrcodeScreenState extends State<QrcodeScreen> {
             actions: <Widget>[
               TextButton(
                 onPressed: () => Navigator.pop(context, 'OK'),
-                child:
-                const Text('OK', style: TextStyle(color: Color(0xff803b7a))),
+                child: const Text('OK',
+                    style: TextStyle(color: Color(0xff803b7a))),
               ),
             ],
           ),
         );
       }
-
-
     }
   }
+
   void _screenWasClosed() {
     _screenOpened = false;
   }
 }
-
-
-
